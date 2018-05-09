40b27f15249390690f725cc859dfd3eeda310c26
rockydang <rocky_dang@163.com>
2018-05-09 15:12:01 +0800

    commit pom

--- web/src/main/java/com/rockydang/springbootdemo/web/Application.java
+++ web/src/main/java/com/rockydang/springbootdemo/web/Application.java
@@ -3,18 +3,17 @@ package com.rockydang.springbootdemo.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by yanxiao on 16/4/20.
  */
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+/*
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+//@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application extends SpringBootServletInitializer {
 
     @Override
@@ -24,6 +23,26 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         args = new String[]{"1","2"};
+        SpringApplication.run(Application.class, args);
+    }
+
+}*/
+
+
+/*@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}*/
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
